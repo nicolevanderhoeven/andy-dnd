703d21156fad0b2fdde9152cab2b63547884f258
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2023-05-07 18:10:13 +0800

    Removed logs

--- Scripts/renameNewSession.js
+++ Scripts/renameNewSession.js
@@ -10,18 +10,14 @@ module.exports = async function renameNewSession(params) {
         let folderName = files.children[file].name;
         if (!folderName.includes("00 Previous Session Lister")) {
             if (!folderName.includes("Previous Session Notes")) {
-                // await app.fileManager.renameFile(files.children[file], targetFolder + '/' + folderName);
                 const newGameName = 'S' + params.variables["newGameNum"] + ' ' + moment().format('YYYY-MM-DD');
                 const newFolderPath = notesFolder + '/' + newGameName;
                 app.fileManager.renameFile(files.children[file], newFolderPath);
                 for (const note in files.children[file].children) {
                     const noteName = files.children[file].children[note].name;
-                    console.log('noteName: ' + noteName);
                     const regex = /S(\d+)/g;
-                    console.log(files.children[file]);
                     if (noteName.match(regex)) {
                         const newNotePath = newFolderPath + '/' + newGameName + '.md';
-                        console.log('newNotePath: ' + newNotePath);
                         app.fileManager.renameFile(files.children[file].children[note], newNotePath);
                     }
                 }
